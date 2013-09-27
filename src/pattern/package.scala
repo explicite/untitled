12917973eb318727007271df2fc7cbae410e5ce6
Janek Paw <explicite@windowslive.com>
2013-09-27 07:59:13 +0200

    second commit

---
+++ src/pattern/package.scala
@@ -0,0 +1,20 @@
+import pattern.Tree._
+
+/**
+ * @author Jan Paw
+ *         Date: 27.09.13
+ */
+package object pattern {
+  def main(args: Array[String]) {
+    val exp: Tree = Sum(Sum(Var("x"), Var("x")), Sum(Const(7), Var("y")))
+    val env: Environment = {
+      case "x" => 5
+      case "y" => 7
+    }
+
+    println("Expression: " + exp)
+    println("Evaluation with x=5, y=7: " + eval(exp, env))
+    println("Derivative relative to x:\n " + derive(exp, "x"))
+    println("Derivative relative to y:\n " + derive(exp, "y"))
+  }
+}
