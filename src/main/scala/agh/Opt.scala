0172ddcf2da041a0cb155564369e66b222bc58fe
Jan Paw <explicite@windowslive.com>
2013-11-11 21:49:44 +0100

    lagrange optimization

--- src/main/scala/agh/Opt.scala
+++ src/main/scala/agh/Opt.scala
@@ -1,12 +1,55 @@
 package agh
 
 import scala.annotation.tailrec
+import scala.language.reflectiveCalls
 
 /**
  * @author Jan Paw
  *         Date: 11/9/13
  */
 object Opt {
+
+  def lagrange(f: Double => Double, a: Double, b: Double, ε: Double, γ: Double, N: Double): (Double, Double) = {
+
+    var ai = a
+    var bi = b
+    var ci = (a + b) / 2
+
+    def d =
+      0.5 * (f(ai) * ((ci * ci) - (bi * bi)) + f(ci) * ((bi * bi) - (ai * ai)) + f(bi) * ((ai * ai) - (ci * ci))) / ((f(ai) * (ci - bi)) + (f(ci) * (bi - ai)) + (f(bi) * (ai - ci)))
+
+    var di = d
+    var i = 0
+
+    repeat {
+
+      di = d
+
+      if (ai < di && di < ci) {
+        if (f(di) < f(ci)) {
+          bi = ci
+          ci = di
+        } else
+          ai = di
+      } else if (ci < d && d < bi) {
+        if (f(di) < f(ci)) {
+          ai = ci
+          ci = di
+        } else
+          bi = di
+      } else
+        throw new Error("algorithm converges!")
+
+      i = i + 1
+      if (i > N)
+        throw new Error("cannot reach ε!")
+
+    } until (bi - ai < ε || Math.abs(d - di) <= γ)
+
+    val x = d
+    (x, f(x))
+
+  }
 
   def fibonacci(f: Double => Double, a: Double, b: Double, ε: Double): (Double, Double) = {
     val k = φ((b - a) / ε)
@@ -40,4 +83,13 @@ object Opt {
     if (φk(k) < d) findFrom(d, k + 1) else k
 
   def φ(d: Double): Int = findFrom(d, 0)
+
+
+  def repeat(body: => Unit) = new {
+    def until(condition: => Boolean) = {
+      do {
+        body
+      } while (!condition)
+    }
+  }
 }
--- src/test/scala/agh/OptTest.scala
+++ src/test/scala/agh/OptTest.scala
@@ -1,6 +1,6 @@
 package agh
 
-import scala.math.sin
+import scala.math.{sin, Pi}
 import org.scalatest.FunSuite
 
 /**
@@ -10,9 +10,21 @@ import org.scalatest.FunSuite
 class OptTest extends FunSuite {
   def f(x: Double) = (6 * x - 2) * (6 * x - 2) * sin(12 * x - 4)
 
+  def trn(d: Double, p: Double) = d - (d % p)
+
   test("min (6x-2)^2sin(12x-4) over [0,1]") {
     val optMin = Opt.fibonacci(f, 0, 1, 0.001)
     assert(optMin._1 == 0.7573530363169201 && optMin._2 == -6.020734247281986)
   }
 
+  test("min sin(x) over [π,2π]") {
+    val optMin = Opt.fibonacci(sin, Pi, 2 * Pi, 0.00001)
+    assert(trn(optMin._1, 0.0001) == trn(3 * Pi / 2, 0.0001))
+  }
+
+  test("min x + 1/x^2 over [1,2] with ε=0.01 and  γ=0.0001") {
+    def f(x: Double): Double = x + (1 / (x * x))
+    val optMin = Opt.lagrange(f, 1, 2, 0.01, 0.0001, 100)
+    assert(trn(optMin._1, 0.0001) == 1.2599)
+  }
 }
